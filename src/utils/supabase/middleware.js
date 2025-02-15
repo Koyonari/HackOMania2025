4e57da8284fd77893f4403359295ad6f7d53b37b
An Yong Shyan <yongshyan.an@gmail.com>
2025-02-15 21:34:57 +0800

    feat: update middleware to access forum page

--- src/utils/supabase/middleware.js
+++ src/utils/supabase/middleware.js
@@ -1,5 +1,5 @@
-import { createServerClient } from '@supabase/ssr'
-import { NextResponse } from 'next/server';
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse } from "next/server";
 
 export async function updateSession(request) {
   // Start by creating a base response that we may modify
@@ -15,7 +15,9 @@ export async function updateSession(request) {
           return request.cookies.getAll();
         },
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value));
+          cookiesToSet.forEach(({ name, value, options }) =>
+            request.cookies.set(name, value)
+          );
           // Create a new response so we can set cookies on it
           supabaseResponse = NextResponse.next({ request });
           cookiesToSet.forEach(({ name, value, options }) =>
@@ -32,42 +34,42 @@ export async function updateSession(request) {
 
   // IMPORTANT: DO NOT run any code between createServerClient and supabase.auth.getUser()
   // Get the user from the auth session.
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  // const {
+  //   data: { user },
+  // } = await supabase.auth.getUser();
 
-  // Query your users table based on the user id.
-  const { data, error } = await supabase
-    .from('users')
-    .select('*')
-    .eq('id', user?.id);
-    if (currentPath.startsWith('/app'))
-    {
-  // If there is no user, redirect to /login if you're not already there.
-        if (!user) {
-            if (currentPath !== '/login') {
-            url.pathname = '/login';
-            return NextResponse.redirect(url);
-            }
-            return NextResponse.next();
-        }
-        // If the user is logged in but no matching row in your users table, redirect to /app/settings
-        else if (user && (!data || data.length === 0)) {
-            if (currentPath !== '/app/settings' && currentPath !== '/app/settings/wallet') {
-            url.pathname = '/app/settings';
-            return NextResponse.redirect(url);
-            }
-            return NextResponse.next();
-        }
-        // If the user is logged in and has a row in the users table, redirect to /app if not already there.
-        else if (user && data && data.length > 0) {
-            if (currentPath !== '/app') {
-            url.pathname = '/app';
-            return NextResponse.redirect(url);
-            }
-            return NextResponse.next();
-        }
-    }
+  // // Query your users table based on the user id.
+  // const { data, error } = await supabase
+  //   .from('users')
+  //   .select('*')
+  //   .eq('id', user?.id);
+  //   if (currentPath.startsWith('/app'))
+  //   {
+  // // If there is no user, redirect to /login if you're not already there.
+  //       if (!user) {
+  //           if (currentPath !== '/login') {
+  //           url.pathname = '/login';
+  //           return NextResponse.redirect(url);
+  //           }
+  //           return NextResponse.next();
+  //       }
+  //       // If the user is logged in but no matching row in your users table, redirect to /app/settings
+  //       else if (user && (!data || data.length === 0)) {
+  //           if (currentPath !== '/app/settings' && currentPath !== '/app/settings/wallet') {
+  //           url.pathname = '/app/settings';
+  //           return NextResponse.redirect(url);
+  //           }
+  //           return NextResponse.next();
+  //       }
+  //       // If the user is logged in and has a row in the users table, redirect to /app if not already there.
+  //       else if (user && data && data.length > 0) {
+  //           if (currentPath !== '/app') {
+  //           url.pathname = '/app';
+  //           return NextResponse.redirect(url);
+  //           }
+  //           return NextResponse.next();
+  //       }
+  //   }
   // Fallback: return the original response if none of the above conditions matched.
   return supabaseResponse;
 }
