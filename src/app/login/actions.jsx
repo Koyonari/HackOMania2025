ea24cecb6eb4ab8fccf0b806825c0c20ae545653
brazeenNP <s10255790@connect.np.edu.sg>
2025-02-15 16:48:31 +0800

    added auth callback

--- src/app/login/actions.jsx
+++ src/app/login/actions.jsx
@@ -1,29 +1,34 @@
 import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+import { redirect } from 'next/navigation'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 export async function signInWithGoogle() {
-  const supabase = await createClient(supabaseUrl,
-    supabaseAnonKey);
+  const supabase = await createClient(supabaseUrl, supabaseAnonKey)
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
-  });
+    options: {
+      redirectTo: `${window.location.origin}/auth/confirm`
+    }
+  })
 
   if (error) {
-    console.error(error);
-    redirect('/error');
+    console.error(error)
+    redirect('/error')
   }
 }
 
 export async function signInWithGithub() {
-  const supabase = await createClient(supabaseUrl, supabaseAnonKey);
+  const supabase = await createClient(supabaseUrl, supabaseAnonKey)
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'github',
-  });
+    options: {
+      redirectTo: `${window.location.origin}/auth/confirm`
+    }
+  })
 
   if (error) {
-    console.error(error);
-    redirect('/error');
+    console.error(error)
+    redirect('/error')
   }
 }
